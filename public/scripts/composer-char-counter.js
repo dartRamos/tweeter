f1173b7794e6576de557fe807a0e83ad89472a30
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-12-15 17:43:12 -0500

    Implemented a character counter that counts down from 140 for every character entered in textarea. If number goes below 0 the number turns red.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // Select the textarea inside the .new-tweet section
+  $('.new-tweet textarea').on('input', function() {
+    
+    // Get current length of text entered
+    let textLength = $(this).val().length;
+
+    // Go to the parent and then find the counter element
+    let counter = $(this).closest('.new-tweet').find(".counter")
+
+    // Calculate the remaining characters (140 - textLength)
+    let remainingChars = 140 - textLength;
+
+    // Update the counter text with the remaining characters
+    counter.text(remainingChars);
+
+    // Turns counter red if number goes below 0
+    if(remainingChars < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', 'black');
+    }
+
+    console.log(remainingChars);
+  });
+});
