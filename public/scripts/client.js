cc9e21072ba0d23ecde8b8867b324f968f606d88
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-12-19 15:42:13 -0500

    Added AJAX GET request to get tweets from server and show them with timeago format (shows how long its been since tweet was created). Removed hard-coded tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  
-  // Sample data array to represent tweets
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   // Function to render the array of tweets
   const renderTweets = function(tweets) {
     // For each tweet in the 'tweets' array, create the tweet element and append it to the page
@@ -43,6 +16,8 @@ $(document).ready(function() {
 
   // Function that creats the HTML structure for a tweet
   const createTweetElement = function(tweet) {
+    // Formate creation time suing timeago.js
+    const timeAgo = timeago.format(new Date(tweet.created_at));
       // Using jQuery to create an HTML structure for the tweet
       let $tweet = $(`<article class="tweet">
         <header class="tweet-header">
@@ -59,7 +34,7 @@ $(document).ready(function() {
         </section>
         <div class="line"></div>
         <footer class="tweet-footer">
-          <span class="created_at">${new Date(tweet.created_at).toLocaleString()}</span>
+          <span class="created_at">${timeAgo}</span>
           <div class="tweet-icons">
             <i class="fa-solid fa-flag flag"></i>
             <i class="fa-solid fa-retweet retweet"></i>
@@ -74,20 +49,36 @@ $(document).ready(function() {
 
   // Function to handle the form submission 
   const submitHandler = function() {
-    // Attach a 'submit' event listener to the form with id 'target'
     $('#target').on("submit", function(event) {
-      event.preventDefault(); // Prevent the default form submission behavior (page refresh)
-
-      // Serialize the form data into a query string format
-      const tweetData = $(this).serialize();
-      console.log(tweetData);
+      event.preventDefault(); // Prevent the default form submission behavior
+      const tweetData = $(this).serialize(); // Serialize the form data
+   
+      $.post('/tweets', tweetData, function(response) {
+        console.log('Server Response:', response); // Log the response to inspect it
+        renderTweets([response]); // Append the newly created tweet to the container
+      });
     });
   }
 
+  // Function to load tweets from server when the page loads
+  const loadTweets = function(){
+    $.ajax({
+      url: '/tweets', // Endpoint to request tweets from
+      method: 'GET', // method to fetch data
+      dataType: 'JSON', // Get a JSON response from server
+      success: function(response) {
+        renderTweets(response)
+      },
+      error: function() {
+        console.log('Error fetrching tweets:', err); // Logs error if something goes wrong
+      }
+    })
+  }
+
+  // Loads tweets when page loads
+  loadTweets();
+  
   // Activate the submitHandler function to listen for form submission events
   submitHandler();
-
-  // Render the initial tweets on the page
-  renderTweets(data);
 });
 
